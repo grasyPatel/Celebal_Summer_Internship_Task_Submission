db91e1bc4aec35dab4ae58e9b26099f72cb11bfe
grasypatel <gracepatel91@gmail.com>
2025-05-21 21:01:38 +0530

    Validate_Form as Task

---
+++ Validate_Form/validate-form/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import FormPage from '../components/FormPage';
+import OutPutPage from '../components/OutputPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<FormPage />} />
+        <Route path="/output" element={<OutPutPage />} />
+
+      </Routes>
+
+    </Router>
+   
+  )
+}
+
+export default App
