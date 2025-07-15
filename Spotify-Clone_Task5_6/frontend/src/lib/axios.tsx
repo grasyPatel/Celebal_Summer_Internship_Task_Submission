76a76bc12fb124f4ee35cb32ecc03792edb858f0
grasypatel <gracepatel91@gmail.com>
2025-07-15 17:38:26 +0530

    setup CRA frontend for Render deployment

--- Spotify-Clone_Task5_6/frontend/src/lib/axios.tsx
+++ Spotify-Clone_Task5_6/frontend/src/lib/axios.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 export const axiosInstance=axios.create({
-    baseURL:"http://localhost:5050/api",
+      baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5050/api",
+
     withCredentials:true
 
 });
